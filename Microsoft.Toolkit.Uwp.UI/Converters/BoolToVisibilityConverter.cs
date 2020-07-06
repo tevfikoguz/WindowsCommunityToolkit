7c32413a551fe2a7ac90a68fbf751925e9d48c9c
Sergio Pedri <sergio0694@live.com>
2020-07-06 20:18:16 +0200

    Refactored BoolToVisibilityConverter type

--- Microsoft.Toolkit.Uwp.UI/Converters/BoolToVisibilityConverter.cs
+++ Microsoft.Toolkit.Uwp.UI/Converters/BoolToVisibilityConverter.cs
@@ -2,22 +2,50 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Diagnostics.Contracts;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
 
 namespace Microsoft.Toolkit.Uwp.UI.Converters
 {
     /// <summary>
     /// This class converts a boolean value into a Visibility enumeration.
     /// </summary>
-    public class BoolToVisibilityConverter : BoolToObjectConverter
+    public class BoolToVisibilityConverter : IValueConverter
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="BoolToVisibilityConverter"/> class.
+        /// Convert a <see cref="bool"/> value to a <see cref="Visibility"/> value.
         /// </summary>
-        public BoolToVisibilityConverter()
+        /// <param name="value">The <see cref="bool"/> value to convert.</param>
+        /// <returns>The <see cref="Visibility"/> value for <paramref name="value"/>.</returns>
+        [Pure]
+        public static Visibility Convert(bool value)
         {
-            TrueValue = Visibility.Visible;
-            FalseValue = Visibility.Collapsed;
+            return value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Convert a <see cref="Visibility"/> value to a <see cref="bool"/> value.
+        /// </summary>
+        /// <param name="value">The <see cref="Visibility"/> value to convert.</param>
+        /// <returns>The <see cref="bool"/> value for <paramref name="value"/>.</returns>
+        [Pure]
+        public bool ConvertBack(Visibility value)
+        {
+            return value == Visibility.Visible;
+        }
+
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return Convert((value as bool?).GetValueOrDefault());
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return ConvertBack((value as Visibility?).GetValueOrDefault());
         }
     }
 }
