f13d204711b5446a58b3bb6f3cce23f90026f3c4
Sergio Pedri <sergio0694@live.com>
2020-07-06 22:32:45 +0200

    Refactored CollectionVisibilityConverter type

--- Microsoft.Toolkit.Uwp.UI/Converters/CollectionVisibilityConverter.cs
+++ Microsoft.Toolkit.Uwp.UI/Converters/CollectionVisibilityConverter.cs
@@ -2,22 +2,80 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
 
 namespace Microsoft.Toolkit.Uwp.UI.Converters
 {
     /// <summary>
     /// This class converts a collection size to visibility.
     /// </summary>
-    public class CollectionVisibilityConverter : EmptyCollectionToObjectConverter
+    public class CollectionVisibilityConverter : IValueConverter
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="CollectionVisibilityConverter"/> class.
+        /// Convert a <see cref="bool"/> value to its negation.
         /// </summary>
-        public CollectionVisibilityConverter()
+        /// <param name="value">The <see cref="bool"/> value to negate.</param>
+        /// <returns>The negation of <paramref name="value"/>.</returns>
+        [Pure]
+        public static Visibility Convert(IEnumerable value)
         {
-            NotEmptyValue = Visibility.Visible;
-            EmptyValue = Visibility.Collapsed;
+            return Any(value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return Any(value as IEnumerable) ? ConverterTools.Visible : ConverterTools.Collapsed;
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks whether the input source contains at least one item.
+        /// </summary>
+        /// <param name="source">The input source to inspect.</param>
+        /// <returns>Whether or not <paramref name="source"/> has at least one item.</returns>
+        [Pure]
+        private static bool Any(IEnumerable source)
+        {
+            if (source is null)
+            {
+                return false;
+            }
+
+            if (source is ICollection<object> collectionOfT)
+            {
+                return collectionOfT.Count != 0;
+            }
+
+            if (source is IReadOnlyCollection<object> readOnlyCollectionOfT)
+            {
+                return readOnlyCollectionOfT.Count != 0;
+            }
+
+            if (source is ICollection collection)
+            {
+                return collection.Count != 0;
+            }
+
+            IEnumerator enumerator = source.GetEnumerator();
+            try
+            {
+                return enumerator.MoveNext();
+            }
+            finally
+            {
+                (enumerator as IDisposable)?.Dispose();
+            }
         }
     }
 }
