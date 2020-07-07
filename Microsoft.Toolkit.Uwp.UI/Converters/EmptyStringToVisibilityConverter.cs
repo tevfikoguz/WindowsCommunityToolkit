66b9cf9de0ec03f10a1f414adfcb46b9af3e895c
Sergio Pedri <sergio0694@live.com>
2020-07-07 12:35:35 +0200

    Added EmptyStringToVisibilityConverter type

---
+++ Microsoft.Toolkit.Uwp.UI/Converters/EmptyStringToVisibilityConverter.cs
@@ -0,0 +1,40 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Diagnostics.Contracts;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Microsoft.Toolkit.Uwp.UI.Converters
+{
+    /// <summary>
+    /// This class converts a string value into a Visibility value (if the value is null or empty returns a collapsed value).
+    /// </summary>
+    public class EmptyStringToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert a <see cref="bool"/> value to its negation.
+        /// </summary>
+        /// <param name="value">The <see cref="bool"/> value to negate.</param>
+        /// <returns>The negation of <paramref name="value"/>.</returns>
+        [Pure]
+        public static Visibility Convert(string value)
+        {
+            return string.IsNullOrEmpty(value) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return string.IsNullOrEmpty(value as string) ? ConverterTools.Visible : ConverterTools.Collapsed;
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
