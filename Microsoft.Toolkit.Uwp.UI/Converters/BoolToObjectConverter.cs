1d2d6907206f1c8f5f05551e9ae3c96dc1f7f8eb
Sergio Pedri <sergio0694@live.com>
2020-07-07 12:38:42 +0200

    Refactored BoolToObjectConverter type

--- Microsoft.Toolkit.Uwp.UI/Converters/BoolToObjectConverter.cs
+++ Microsoft.Toolkit.Uwp.UI/Converters/BoolToObjectConverter.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Diagnostics.Contracts;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 
@@ -15,75 +16,87 @@ namespace Microsoft.Toolkit.Uwp.UI.Converters
     public class BoolToObjectConverter : DependencyObject, IValueConverter
     {
         /// <summary>
+        /// Gets or sets the value to be returned when the boolean is true.
+        /// </summary>
+        public object TrueValue
+        {
+            get => GetValue(TrueValueProperty);
+            set => SetValue(TrueValueProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the value to be returned when the boolean is false.
+        /// </summary>
+        public object FalseValue
+        {
+            get => GetValue(FalseValueProperty);
+            set => SetValue(FalseValueProperty, value);
+        }
+
+        /// <summary>
         /// Identifies the <see cref="TrueValue"/> property.
         /// </summary>
-        public static readonly DependencyProperty TrueValueProperty =
-            DependencyProperty.Register(nameof(TrueValue), typeof(object), typeof(BoolToObjectConverter), new PropertyMetadata(null));
+        public static readonly DependencyProperty TrueValueProperty = DependencyProperty.Register(
+            nameof(TrueValue),
+            typeof(object),
+            typeof(BoolToObjectConverter),
+            new PropertyMetadata(null));
 
         /// <summary>
         /// Identifies the <see cref="FalseValue"/> property.
         /// </summary>
-        public static readonly DependencyProperty FalseValueProperty =
-            DependencyProperty.Register(nameof(FalseValue), typeof(object), typeof(BoolToObjectConverter), new PropertyMetadata(null));
-
-        /// <summary>
-        /// Gets or sets the value to be returned when the boolean is true
-        /// </summary>
-        public object TrueValue
-        {
-            get { return GetValue(TrueValueProperty); }
-            set { SetValue(TrueValueProperty, value); }
-        }
-
-        /// <summary>
-        /// Gets or sets the value to be returned when the boolean is false
-        /// </summary>
-        public object FalseValue
-        {
-            get { return GetValue(FalseValueProperty); }
-            set { SetValue(FalseValueProperty, value); }
-        }
+        public static readonly DependencyProperty FalseValueProperty = DependencyProperty.Register(
+            nameof(FalseValue),
+            typeof(object),
+            typeof(BoolToObjectConverter),
+            new PropertyMetadata(null));
 
         /// <summary>
         /// Convert a boolean value to an other object.
         /// </summary>
-        /// <param name="value">The source data being passed to the target.</param>
-        /// <param name="targetType">The type of the target property, as a type reference.</param>
-        /// <param name="parameter">An optional parameter to be used to invert the converter logic.</param>
-        /// <param name="language">The language of the conversion.</param>
+        /// <param name="value">The input <see cref="bool"/> value.</param>
+        /// <param name="invert">Whether or not to invert <paramref name="value"/>.</param>
         /// <returns>The value to be passed to the target dependency property.</returns>
-        public object Convert(object value, Type targetType, object parameter, string language)
+        [Pure]
+        public object Convert(bool value, bool invert)
         {
-            bool boolValue = value is bool && (bool)value;
-
-            // Negate if needed
-            if (ConverterTools.TryParseBool(parameter))
-            {
-                boolValue = !boolValue;
-            }
-
-            return ConverterTools.Convert(boolValue ? TrueValue : FalseValue, targetType);
+            return (value && !invert) || (!value && invert)
+                ? TrueValue
+                : FalseValue;
         }
 
         /// <summary>
-        /// Convert back the value to a boolean
+        /// Convert back the value to a boolean.
         /// </summary>
+        /// <param name="value">The input <see cref="bool"/> value.</param>
+        /// <param name="invert">Whether or not to invert <paramref name="value"/>.</param>
+        /// <returns>The value to be passed to the target dependency property.</returns>
         /// <remarks>If the <paramref name="value"/> parameter is a reference type, <see cref="TrueValue"/> must match its reference to return true.</remarks>
-        /// <param name="value">The target data being passed to the source.</param>
-        /// <param name="targetType">The type of the target property, as a type reference (System.Type for Microsoft .NET, a TypeName helper struct for Visual C++ component extensions (C++/CX)).</param>
-        /// <param name="parameter">An optional parameter to be used to invert the converter logic.</param>
-        /// <param name="language">The language of the conversion.</param>
-        /// <returns>The value to be passed to the source object.</returns>
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        [Pure]
+        public bool ConvertBack(object value, bool invert)
         {
-            bool result = Equals(value, ConverterTools.Convert(TrueValue, value.GetType()));
+            bool result = ReferenceEquals(value, TrueValue);
 
-            if (ConverterTools.TryParseBool(parameter))
+            if (invert)
             {
                 result = !result;
             }
 
             return result;
+        }
+
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return Convert(
+                (value as bool?).GetValueOrDefault(),
+                ConverterTools.TryParseBool(parameter));
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return ConvertBack(value, ConverterTools.TryParseBool(parameter));
         }
     }
 }
